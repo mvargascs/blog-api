31980a4d5710d95d92bd6b8c4f901da08ac281ee
Miguel Vargas <mvargasuo@gmail.com>
2020-01-14 16:09:16 -0500

    Added CORS policy to all routes to enable CORS.

--- src/main/java/com/example/blogrest/BlogRestApplication.java
+++ src/main/java/com/example/blogrest/BlogRestApplication.java
@@ -2,6 +2,9 @@ package com.example.blogrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BlogRestApplication {
@@ -10,4 +13,13 @@ public class BlogRestApplication {
 		SpringApplication.run(BlogRestApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
+	}
 }
